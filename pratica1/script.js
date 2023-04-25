5e532ebe8f89d2a7f4370281afe83d27a539d308
brunavabs <bru.almeida12@gmail.com>
2023-04-24 21:36:54 -0300

    exercicios resolvidos

---
+++ pratica1/script.js
@@ -0,0 +1,25 @@
+const sapo = document.getElementById('sapo')
+
+console.log(sapo)
+console.log(sapo.innerHTML)
+
+//reatribuo algum valor ao inner.HTML
+sapo.innerHTML += ' pé'
+
+
+const poema = document.getElementById('poema')
+console.log(poema.innerHTML)
+poema.innerHTML += ' evidências'
+
+
+const sabedoria = document.getElementById('sabedoria')
+console.log(sabedoria.innerHTML)
+sabedoria.innerHTML += ' voando'
+
+const borboleta = document.getElementById('borboleta')
+console.log(borboleta.innerHTML)
+borboleta.innerHTML += ' pica-pau'
+
+const roberto = document.getElementById('roberto')
+console.log(roberto.innerHTML)
+roberto.innerHTML += ' vivi'
---
+++ pratica2/script.js
@@ -0,0 +1,15 @@
+// capturando os elementos HTML
+const inputNome = document.getElementById('nome')
+// imprimindo o value do input
+console.log(inputNome)
+//limpando o input
+inputNome.value = ''
+
+const inputEndereco = document.getElementById('endereco')
+console.log(inputEndereco)
+inputEndereco.value = ''
+
+const inputEmail = document.getElementById('email')
+console.log(inputEmail)
+inputEmail.value = ''
+
